5f817442eb60ecd345641a7c4f409b38cc4c9627
antorrg <antorrg>
2026-04-19 23:34:19 -0300

    probando desactivar ratelimit

--- server/Features/auth/auth.routes.ts
+++ server/Features/auth/auth.routes.ts
@@ -1,14 +1,17 @@
 import express from 'express'
 import { AuthService } from './AuthService.js'
 import { AuthController } from './AuthController.js'
-import { RateLimiter } from '../../Shared/Middlewares/RateLimiter.js'
+//import { RateLimiter } from '../../Shared/Middlewares/RateLimiter.js'
 
 const authService = new AuthService()
 const authController = new AuthController(authService)
 
 const authRouter = express.Router()
 
-authRouter.post('/login', RateLimiter.loginRateLimiter, authController.login)
+authRouter.post(
+    '/login', 
+    //RateLimiter.loginRateLimiter, 
+    authController.login)
 authRouter.post('/logout', authController.logout)
 authRouter.get('/me', authController.me)
 
